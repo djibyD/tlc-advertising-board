46eb4aad99fe283e9c719bcb488ed6a11b3afeab
djibyD <djialldji@gmail.com>
2017-02-08 14:14:29 +0100

    first commit

---
+++ src/servlet/AdvertisingBoardServlet.java
@@ -0,0 +1,35 @@
+package servlet;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class AdvertisingBoardServlet
+ */
+@WebServlet("/AdvertisingBoardServlet")
+public class AdvertisingBoardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public AdvertisingBoardServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
